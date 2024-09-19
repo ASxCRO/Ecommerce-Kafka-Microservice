209bdb86ca1e43b4dc7f56524b04e5fe1966b04a
ASxCRO <antonio.suups@gmail.com>
2024-09-19 21:19:51 +0200

    Add project files.

---
+++ Ecommerce.OrderService/Controllers/OrderController.cs
@@ -0,0 +1,37 @@
+﻿using Ecommerce.Model;
+using Ecommerce.OrderService.Data;
+using Ecommerce.OrderService.Kafka;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
+namespace Ecommerce.OrderService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController(OrderDbContext orderDbContext, IKafkaProducer kafkaProducer) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<List<OrderModel>> GetOrders()
+        {
+            return await orderDbContext.Orders.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<OrderModel> Create(OrderModel model)
+        {
+            model.OrderDate = DateTime.Now;
+            await orderDbContext.Orders.AddAsync(model);
+            await orderDbContext.SaveChangesAsync();
+            
+            await kafkaProducer.ProduceAsync("order-topic", new Confluent.Kafka.Message<string, string>
+            {
+                Key=model.Id.ToString(),
+                Value=System.Text.Json.JsonSerializer.Serialize(model)
+            });
+
+            return model;
+        }
+    }
+}
---
+++ Ecommerce.ProductService/Data/ProductDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Ecommerce.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.ProductService.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductModel>().HasData(new ProductModel { Id=1,Name="Shirt",Quantity=20,Price= 20 });
+            modelBuilder.Entity<ProductModel>().HasData(new ProductModel { Id=2,Name="Pant",Quantity=50,Price= 10});
+            modelBuilder.Entity<ProductModel>().HasData(new ProductModel { Id=3,Name="Glasses",Quantity=100,Price= 5 });
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<ProductModel> Products { get; set; }
+    }
+}
